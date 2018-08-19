ef518de26ad581f0bf2d23d1b7db93b96f481b94
Leo Chan <leo.chan@tokuindustry.com>
2018-08-18 23:03:44 -0600

    MOdified hello example to specify name of bot.

--- skills/hello.js
+++ skills/hello.js
@@ -11,14 +11,18 @@ module.exports = function(controller) {
   // See: https://botkit.ai/docs/readme-studio.html#controllerstudiobefore
   controller.studio.before('hello', function(convo, next) {
 
-      // do some preparation before the conversation starts...
-      // for example, set variables to be used in the message templates
-      // convo.setVar('foo','bar');
+    // do some preparation before the conversation starts...
+    // for example, set variables to be used in the message templates
+    // convo.setVar('foo','bar');
+    console.log('BEFORE: hello');
+    convo.context.bot.profile.then(p => {
+      convo.setVar('bot', {
+        name: p.full_name
+      });
 
-      console.log('BEFORE: hello');
       // don't forget to call next, or your conversation will never continue.
       next();
-
+    });
   });
 
   /* Validators */
@@ -29,45 +33,45 @@ module.exports = function(controller) {
   // Validate user input: question_1
   controller.studio.validate('hello','question_1', function(convo, next) {
 
-      var value = convo.extractResponse('question_1');
+    var value = convo.extractResponse('question_1');
 
-      // test or validate value somehow
-      // can call convo.gotoThread() to change direction of conversation
+    // test or validate value somehow
+    // can call convo.gotoThread() to change direction of conversation
 
-      console.log('VALIDATE: hello VARIABLE: question_1');
+    console.log('VALIDATE: hello VARIABLE: question_1');
 
-      // always call next!
-      next();
+    // always call next!
+    next();
 
   });
 
   // Validate user input: question_2
   controller.studio.validate('hello','question_2', function(convo, next) {
 
-      var value = convo.extractResponse('question_2');
+    var value = convo.extractResponse('question_2');
 
-      // test or validate value somehow
-      // can call convo.gotoThread() to change direction of conversation
+    // test or validate value somehow
+    // can call convo.gotoThread() to change direction of conversation
 
-      console.log('VALIDATE: hello VARIABLE: question_2');
+    console.log('VALIDATE: hello VARIABLE: question_2');
 
-      // always call next!
-      next();
+    // always call next!
+    next();
 
   });
 
   // Validate user input: question_3
   controller.studio.validate('hello','question_3', function(convo, next) {
 
-      var value = convo.extractResponse('question_3');
+    var value = convo.extractResponse('question_3');
 
-      // test or validate value somehow
-      // can call convo.gotoThread() to change direction of conversation
+    // test or validate value somehow
+    // can call convo.gotoThread() to change direction of conversation
 
-      console.log('VALIDATE: hello VARIABLE: question_3');
+    console.log('VALIDATE: hello VARIABLE: question_3');
 
-      // always call next!
-      next();
+    // always call next!
+    next();
 
   });
 
@@ -79,25 +83,25 @@ module.exports = function(controller) {
   // Before the default thread starts, run this:
   controller.studio.beforeThread('hello','default', function(convo, next) {
 
-      /// do something fun and useful
-      // convo.setVar('name','value');
+    /// do something fun and useful
+    // convo.setVar('name','value');
 
-      console.log('In the script *hello*, about to start the thread *default*');
+    console.log('In the script *hello*, about to start the thread *default*');
 
-      // always call next!
-      next();
+    // always call next!
+    next();
   });
 
   // Before the on_timeout thread starts, run this:
   controller.studio.beforeThread('hello','on_timeout', function(convo, next) {
 
-      /// do something fun and useful
-      // convo.setVar('name','value');
+    /// do something fun and useful
+    // convo.setVar('name','value');
 
-      console.log('In the script *hello*, about to start the thread *on_timeout*');
+    console.log('In the script *hello*, about to start the thread *on_timeout*');
 
-      // always call next!
-      next();
+    // always call next!
+    next();
   });
 
 
@@ -106,17 +110,15 @@ module.exports = function(controller) {
   // See: https://botkit.ai/docs/readme-studio.html#controllerstudioafter
   controller.studio.after('hello', function(convo, next) {
 
-      console.log('AFTER: hello');
+    console.log('AFTER: hello');
 
-      // handle the outcome of the convo
-      if (convo.successful()) {
+    // handle the outcome of the convo
+    if (convo.successful()) {
+      var responses = convo.extractResponses();
+      // do something with the responses
+    }
 
-          var responses = convo.extractResponses();
-          // do something with the responses
-
-      }
-
-      // don't forget to call next, or your conversation will never properly complete.
-      next();
+    // don't forget to call next, or your conversation will never properly complete.
+    next();
   });
-}
+};
